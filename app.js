085b1dc5629446eb349e73d83ddc8ae38816ac74
Teerin Leepaiboon <L.Teerin@gmail.com>
2020-06-09 23:30:45 +0700

    fix error

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+// Reply with two static messages
 
-
-const app = express();
-const port = process.env.PORT || 4000;
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-
-app.post('/webhook',(req,res) => {
-    let reply_token = req.body.events[0].replyToken;
-    reply(reply_token);
-    res.sendStatus(200);
-});
-app.listen(port);
-
-function reply(reply_token){
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token) {
     let headers = {
-        'Content-type': 'application/json',
-        'Authentication': 'Bearer GBrEiGkGX0EZnU39JQZPJbCx7ui1c1u3/FvRKp3v0tQWEyEQa4Ob1Bgq+ZbjnZbgNqwyZA38gKPU1XC5DIu4VoprUL1cvFWwLDzfwXzP45n/zHRZ+Mi9JYbNuZetPzJKTctCot2iUDqS8B/2w4ZPJwdB04t89/1O/w1cDnyilFU='
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer GBrEiGkGX0EZnU39JQZPJbCx7ui1c1u3/FvRKp3v0tQWEyEQa4Ob1Bgq+ZbjnZbgNqwyZA38gKPU1XC5DIu4VoprUL1cvFWwLDzfwXzP45n/zHRZ+Mi9JYbNuZetPzJKTctCot2iUDqS8B/2w4ZPJwdB04t89/1O/w1cDnyilFU='
     }
-
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
