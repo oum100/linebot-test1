e9798dc09cbd8d15555a3f38c07bc671a98a3fb5
Teerin Leepaiboon <L.Teerin@gmail.com>
2020-06-10 00:36:59 +0700

    fix error

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken;
-    let lineevent = req.body;
-    console.log(lineevent);
+    let line_body = req.body;
+    console.log(line_body);
     console.log();
-    console.log(lineevent.events[0]);
+    let line_event = req.body.events[0];
+    console.log(line_event);
+    let reply_token = req.body.events[0].replyToken;
     reply(reply_token);
     res.sendStatus(200);
 })
