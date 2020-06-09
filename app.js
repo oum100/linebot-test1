49ee2b9085fc96f86761f54710a19bd2b98b730a
Teerin Leepaiboon <L.Teerin@gmail.com>
2020-06-10 00:01:32 +0700

    fix error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    let lineevent = req.body.events[0]
+    let lineevent = req.body
     console.log(lineevent)
     reply(reply_token)
     res.sendStatus(200)
