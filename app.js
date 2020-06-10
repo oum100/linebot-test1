bf40a1edfc7afc51207c068041726c85ee7f7ee4
Teerin Leepaiboon <L.Teerin@gmail.com>
2020-06-10 23:43:36 +0700

    Update

--- app.js
+++ app.js
@@ -35,17 +35,44 @@ function reply(reply_token,menu) {
         'Authorization': 'Bearer GBrEiGkGX0EZnU39JQZPJbCx7ui1c1u3/FvRKp3v0tQWEyEQa4Ob1Bgq+ZbjnZbgNqwyZA38gKPU1XC5DIu4VoprUL1cvFWwLDzfwXzP45n/zHRZ+Mi9JYbNuZetPzJKTctCot2iUDqS8B/2w4ZPJwdB04t89/1O/w1cDnyilFU='
     }
 
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: 'กำลังใช้เครื่องไหนบ้างคะ (Which machine you using?)'
-        },
-        {
-            type: 'text',
-            text: 'เช่น 93 100 (Ex. 93 100)'
-        }]
-    })
+    let body="";
+    if(menu == 1){
+        body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: 'กำลังใช้เครื่องไหนบ้างคะ (Which machine you using?)'
+            },
+            {
+                type: 'text',
+                text: 'เช่น 93 100 (Ex. 93 100)'
+            }]
+        })
+    }
+
+    if(menu == 2){
+        body =JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: 'จองคิวเครื่องไหนบ้างคะ (Which machine would you like to book?)'
+            },
+            {
+                type: 'text',
+                text: 'เช่น 93 100 (Ex. 93 100)'
+            }]
+        })
+    }
+
+    if(menu == 3){
+        body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: 'กรุณารอสักครู่ (Wait a minute.)'
+            }]
+        })
+    }
 
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
