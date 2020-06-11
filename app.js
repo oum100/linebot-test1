65c44bf8026f907e0ffd8bb02a3fdb307aed68ce
Teerin Leepaiboon <L.Teerin@gmail.com>
2020-06-11 14:18:04 +0700

    Update

--- app.js
+++ app.js
@@ -8,23 +8,15 @@ const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
-    console.log(req.body);
-    console.log();
+    //console.log(req.body);
+    //console.log();
     console.log(req.body.events);
 
     let reply_token = req.body.events[0].replyToken;
     let menu_msg = req.body.events[0].message.text;
 
-    if(menu_msg[0]==1) 
-        console.log("Menu: "+ menu_msg[0]);
-
-    if(menu_msg[0]==2) 
-        console.log("Menu: "+ menu_msg[0]);
-
-    if(menu_msg[0]==3) 
-        console.log("Menu: "+ menu_msg[0]);            
-
-    reply(reply_token,menu_msg[0]);
+    console.log(menu_msg);
+    reply(reply_token,menu_msg);
     res.sendStatus(200);
 })
 app.listen(port)
@@ -35,36 +27,30 @@ function reply(reply_token,menu) {
         'Authorization': 'Bearer GBrEiGkGX0EZnU39JQZPJbCx7ui1c1u3/FvRKp3v0tQWEyEQa4Ob1Bgq+ZbjnZbgNqwyZA38gKPU1XC5DIu4VoprUL1cvFWwLDzfwXzP45n/zHRZ+Mi9JYbNuZetPzJKTctCot2iUDqS8B/2w4ZPJwdB04t89/1O/w1cDnyilFU='
     }
 
-    let body="";
-    if(menu == 1){
-        body = JSON.stringify({
+    //let body="";
+    if(menu[0] == "N" || menu[0] == "n"){
+        let assetid = menu.substr(1,3);
+        let body = JSON.stringify({
             replyToken: reply_token,
             messages: [{
                 type: 'text',
-                text: 'กำลังใช้เครื่องไหนบ้างคะ (Which machine you using?)'
-            },
-            {
-                type: 'text',
-                text: 'เช่น 93 100 (Ex. 93 100)'
+                text: 'ตั้งแจ้งเตือน'+assetid;
             }]
         })
     }
 
-    if(menu == 2){
-        body =JSON.stringify({
+    if(menu[0] == "B" ||menu[0]=="b"){
+        let assetid = menu.substr(1,3);
+        let body =JSON.stringify({
             replyToken: reply_token,
             messages: [{
                 type: 'text',
-                text: 'จองคิวเครื่องไหนบ้างคะ (Which machine would you like to book?)'
-            },
-            {
-                type: 'text',
-                text: 'เช่น 93 100 (Ex. 93 100)'
+                text: 'จองคิวเครื่อง' + assetid
             }]
         })
     }
 
-    if(menu == 3){
+    if(menu[0] == 3){
         body = JSON.stringify({
             replyToken: reply_token,
             messages: [{
